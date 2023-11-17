f03ebcfee3a2d570684d2b3700fe1a11235a42d3
Enderr2357 <y2594254561@outlook.com>
2023-11-18 01:41:49 +0800

    渲染用户信息，更新用户头像,更新用户信息

--- src/types/params.d.ts
+++ src/types/params.d.ts
@@ -17,3 +17,33 @@ declare type LoginParams = {
   // encryptedData?: string
   // iv?: string
 }
+
+/** 个人信息用户详情信息 */
+declare type ProfileDetail = {
+  /** 用户ID */
+  id: number
+  /** 头像  */
+  avatar: string
+  /** 账户名  */
+  account: string
+  /** 昵称 */
+  nickname?: string
+  /** 性别 */
+  gender?: Gender
+  /** 生日 */
+  birthday?: string
+  /** 省市区 */
+  fullLocation?: string
+  /** 职业 */
+  profession?: string
+}
+/** 性别 */
+declare type Gender = '女' | '男'
+declare type ProfileParams = Pick<ProfileDetail, 'nickname' | 'gender' | 'birthday' | 'profession'> & {
+  /** 省份编码 */
+  provinceCode?: string
+  /** 城市编码 */
+  cityCode?: string
+  /** 区/县编码 */
+  countyCode?: string
+}
